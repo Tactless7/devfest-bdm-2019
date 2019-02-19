7327e8673fe7f08e1c731867c7d9980ad92fa5cb
Tactless7 <blier.juliane@gmail.com>
2019-02-19 13:42:43 +0100

    Added greetings message

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <div>
+  <div class="display">
     <img src="@/assets/pallet_town.png">
+
+    <div class="message">
+      <p>{{greetings}}</p>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      greetings: 'Hé ! Bonjour, quel est ton nom ?',
+    };
+  },
+};
 </script>
 
 <style>
+@font-face {
+  font-family: pokemon;
+  src: url(/fonts/pokemon-font.woff);
+}
+
 img {
   width: 800px;
   margin-left: 2px;
   image-rendering: pixelated;
+}
+
+.display {
+  position: relative;
+  font-family: pokemon;
+}
+
+.display .message {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background: #fff;
+  margin: 20px;
+  padding: 20px;
 }
 </style>
